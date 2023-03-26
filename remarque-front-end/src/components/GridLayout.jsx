1a0ee11e755e16114e9e2802158873f816f5bb5f
Jakob Dayton <jakobdayton@gmail.com>
2023-03-26 01:05:39 -0400

    stuff

--- remarque-front-end/src/components/GridLayout.jsx
+++ remarque-front-end/src/components/GridLayout.jsx
@@ -1,4 +1,6 @@
-import { Responsive as ResponsiveGridLayout } from "react-grid-layout";
+import { Responsive, WidthProvider } from "react-grid-layout";
+
+const ResponsiveGridLayout = WidthProvider(Responsive);
 
 const styles = {
   border: {
@@ -8,44 +10,27 @@ const styles = {
 };
 
 const MyFirstGrid = () => {
-  const layout = [
-    { i: "a", x: 0, y: 0, w: 6, h: 2, static: "true"},
-    { i: "b", x: 0, y: 0, w: 6, h: 2, static: "true"},
-    { i: "c", x: 0, y: 0, w: 6, h: 2, static: "true"},
-    { i: "d", x: 0, y: 0, w: 6, h: 2, static: "true"},
-    { i: "e", x: 0, y: 0, w: 6, h: 2, static: "true"},
-    { i: "f", x: 0, y: 0, w: 3, h: 2, static: "true"},
-    { i: "g", x: 0, y: 0, w: 3, h: 2, static: "true"}
-];
+  // const [layoutState, setLayoutState] = useState(['John', 'Grace', 'Jared']);
+
+  const layout = {lg:[
+    { i: "a", x: 0, y: 0, w: 6, h: 2},
+    { i: "b", x: 3, y: 4, w: 6, h: 4},
+]
+};
   return (
     <ResponsiveGridLayout
       className="layout"
       layout={layout}
       breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-      cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+      cols={{ lg: 4, md: 10, sm: 6, xs: 4, xxs: 2 }}
       margin={[0, 0]}
-      width={1200}
+      compactType= {"horizontal"}
     >
       <div key="a" style={styles.border}>
-        Monday
+        a
       </div>
       <div key="b" style={styles.border}>
-        Tuesday
-      </div>
-      <div key="c" style={styles.border}>
-        Wednesday
-      </div>
-      <div key="d" style={styles.border}>
-        Thursday
-      </div>
-      <div key="e" style={styles.border}>
-        Friday
-      </div>
-      <div key="f" style={styles.border}>
-        Saturday
-      </div>
-      <div key="g" style={styles.border}>
-        Sunday
+        b
       </div>
     </ResponsiveGridLayout>
   );
---
+++ remarque-front-end/src/components/Toolkit.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Toolkit = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default Toolkit;
